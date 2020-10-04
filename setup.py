c5aaa3087a58f0703b334732d44980f34eb4d8b5
Matias Fontanini <matias.fontanini@gmail.com>
2020-10-04 14:00:23 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cellwars',
+      version='0.0.1',
+      description='The official Python Bot SDK for https://cellwars.io/',
+      url='http://github.com/mfontanini/cellwars-python',
+      author='Matias Fontanini',
+      author_email='matias.fontanini@gmail.com',
+      license='MIT',
+      py_modules=['cellwars'],
+      zip_safe=False)
